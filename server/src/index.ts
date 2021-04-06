8a53b479fd73e750794681c6a59864349759fe1d
Yuan <lsko1332@jinjoosoft.com>
2021-04-06 15:47:46 +0900

    feat: Setup development env
    # Client
    Vue + TypeScript
    
    # Server
    Socket.io + TypeScript

---
+++ server/src/index.ts
@@ -0,0 +1,9 @@
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const httpServer = createServer();
+const io = new Server(httpServer, {});
+
+io.on("connection", () => {});
+
+httpServer.listen(3000);
